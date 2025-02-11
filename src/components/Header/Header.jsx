4cb45fdb1b453271738d17035e32db41304b81ef
bayansuluu <bayanamanapbaeva@gmail.com>
2025-02-11 23:28:57 +0600

    add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import scss from "./Header.module.scss";
+import logo from "../Header/assets/logo.svg";
+import Link from "next/link";
+import Image from "next/image";
+import { arr, links } from "./Header.js";
 
 function Header() {
+  let navs = arr.map(({ title, link }, index) => (
+    <nav key={index}>
+      <Link className={scss.link} href={{ link }}>
+        {title}
+      </Link>
+    </nav>
+  ));
+
+  let aut = links.map(({ title, link }, index) => (
+    <div className={scss.aut} key={index}>
+      <Link className={scss.btns} href={{ link }}>
+        {title}
+      </Link>
+    </div>
+  ));
+
   return (
-    <div>Header</div>
-  )
+   <header className={scss.header}>
+     <div className={scss.main}>
+      <Link href={"/"}><Image src={logo} alt="" /></Link>
+      <nav>{navs}</nav>
+      <aside> {aut}</aside>
+    </div>
+    <hr />
+   </header>
+  );
 }
 
-export default Header
+export default Header;
