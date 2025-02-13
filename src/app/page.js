46437c1b4a632d4522c04404fc739e3a85fde522
bayansuluu <bayanamanapbaeva@gmail.com>
2025-02-13 19:51:12 +0600

    add dishes

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header.jsx";
 import Restaurant from "@/components/Restaurant/Restaurant.jsx"
 import Get50 from "@/components/Get50/Get50";
-import Dishes from "@/components/Dishes/Dishes";
+import Dishes from "@/components/Dishes/Dishes.jsx";
 
 export default function Home() {
   return (
