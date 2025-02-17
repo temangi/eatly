8a2ae6f47b8b37566e28ff776ed1c32e4629f33b
Janara524 <janamanatova.07@gmail.com>
2025-02-17 19:11:11 +0600

    purhasess

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Header from "@/components/Header/Header.jsx";
 import Main from "@/components/Main/Main";
 import Restaurant from "@/components/Restaurant/Restaurant.jsx"
 import Get50 from "@/components/Get50/Get50";
+import Chicken from "@/components/Chicken/Chicken";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
    <Main/>
    <Menu/>
    <Restaurant/>
+   <Chicken/>
    <Get50/>
    <Footer/>
    </>
