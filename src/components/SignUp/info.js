0b3ce90a2cc0fa5fa87f6661b8ddd49db6be28e1
bayansuluu <bayanamanapbaeva@gmail.com>
2025-02-19 19:34:55 +0600

    add signUp

---
+++ src/components/SignUp/info.js
@@ -0,0 +1,26 @@
+import apple from '@/assets/signUp/apple.svg'
+import google from '@/assets/signUp/google.svg'
+import userIcon from '@/assets/signUp/userIcon.svg'
+import email from '@/assets/signUp/email.svg'
+import password from '@/assets/signUp/password.svg'
+import seen from '@/assets/signUp/see.svg'
+
+
+export const arr = [google,apple]
+
+export const arr2 = [
+    {
+        img: userIcon,
+        placeholder: "FULL NAME"
+    },
+    {
+        img: email,
+        placeholder: "EMAIL"
+    },
+    {
+        img: password,
+        placeholder: "PASSWORD",
+        img2: seen
+    }
+    
+]
