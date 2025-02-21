dcc42958e1c12bad88b7c7236e22dbe5f39d17f4
bayansuluu <bayanamanapbaeva@gmail.com>
2025-02-21 19:57:04 +0600

    aut adapt

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,6 +5,7 @@ function page() {
   return (
     <>
     <LogIn/>
+
     </>
   )
 }
--- src/app/signUp/page.js
+++ src/app/signUp/page.js
@@ -1,5 +1,6 @@
 import SignUp from '@/components/SignUp/SignUp'
 import React from 'react'
+import Footer from '@/components/Footer/Footer'
 
 function page() {
   return (
